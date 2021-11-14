f9a4641f7f1e29fdbda34adc30570c636ae35192
ajaezokingsley <ajaezokingsley@gmail.com>
2021-11-14 02:02:50 -0800

    completion of redux auth todo

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import { FaTrash, FaTrashAlt } from 'react-icons/fa';
+import { v4 as uuid } from 'uuid';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { addTodo, removeTodo } from './features/todos/TodoListSlice';
+import { todoArray } from './features/todos/TodoListSlice';
+
+const App = () => {
+  const [todo, setTodo] = useState('');
+
+  const dispatch = useDispatch();
+  const todos = useSelector(todoArray);
+  return (
+    <>
+      <section className="container">
+        <h1>Todo List</h1>
+        <section className="form-div">
+          <div>
+            <input
+              type="text"
+              onChange={e => setTodo(e.target.value)}
+              className="form-control"
+              placeholder="Add a task"
+              value={todo}
+            />
+          </div>
+
+          <div>
+            <button
+              className="btn"
+              onClick={() => {
+                dispatch(addTodo({ id: uuid(), title: todo }));
+                setTodo('');
+              }}
+            >
+              Submit
+            </button>
+          </div>
+        </section>
+        <ul className="todo-list">
+          {todos &&
+            todos.map(todo => {
+              return (
+                <li className="list" key={todo.id}>
+                  <div className="todo-item">
+                    <div>{todo.title}</div>
+                    <div onClick={() => dispatch(removeTodo(todo.id))}>
+                      <FaTrashAlt color="red" />
+                    </div>
+                  </div>
+                </li>
+              );
+            })}
+        </ul>
+      </section>
+    </>
+  );
+};
+
+export default App;
---
+++ src/features/todos/TodoListSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const todoListSlice = createSlice({
+  name: 'todoList',
+  initialState: [],
+
+  reducers: {
+    addTodo(state, action) {
+      state.push(action.payload);
+    },
+    removeTodo(state, action) {
+      state = state.filter(({ id }) => id !== action.payload);
+      return state;
+    },
+  },
+});
+
+export const todoArray = state => state.todoList;
+export const { addTodo, removeTodo } = todoListSlice.actions;
+export default todoListSlice.reducer;
